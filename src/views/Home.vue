1c33dc279ef74298fd7f8c0b0ab6eea3312b7503
vodichka <kimevgenijkimpum@gmail.com>
2021-09-09 09:18:37 +0500

    registration

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <router-link :to="{name: 'registration'}">Sign Up</router-link>
+    <router-link :to="{name: 'login'}">Sign In</router-link>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
   },
 };
 </script>
---
+++ src/views/Registration.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h3 class="teal-text text-lighten-2 center-align">Registration</h3>
+    <div class="row">
+    <form class="col s4 offset-s4">
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="icon_prefix" type="text" class="validate">
+          <label for="icon_prefix">First Name</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+      <button class="waves-effect waves-light btn-large col s6 offset-s3">Submit</button>
+    </form>
+  </div>
+  </div>
+</template>
